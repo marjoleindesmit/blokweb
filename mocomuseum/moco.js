fd76df20c3c60f8781218def8b3b482dfb0c1968
marjoleindesmit <74141033+marjoleindesmit@users.noreply.github.com>
2021-12-01 16:20:24 +0100

    Add files via upload

--- mocomuseum/moco.js
+++ mocomuseum/moco.js
@@ -1,1 +1,16 @@
-var hamburgerMenu;
+var hamburgerMenu = document.querySelector(".hamburger");
+
+
+console.log("Test test test");
+
+
+
+function navMob() {
+  hamburgerMenu.classList.toggle("navOpen");
+}
+
+hamburgerMenu.addEventListener("click", navMob);
+
+
+// BRONNEN
+// https://www.youtube.com/watch?v=dIyVTjJAkLw
